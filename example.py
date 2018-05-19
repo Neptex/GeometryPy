172874ae1f6138467ea03ff8591ea238e515486e
Nerex <34042048+NerexGD@users.noreply.github.com>
2018-05-19 12:00:41 +0000

    Updated

--- example.py
+++ example.py
@@ -1,17 +1,27 @@
 import GeometryPy as GDClient
 
-data = GDClient.GetUserInfo("Nerex")
+Player = GDClient.GetUserInfo("Nerex") #You can also use an AccountID!
+print(Player) 
 
-print(data) # returns { 'username': 'Nerex', 
+""" returns: { 'username': 'Nerex', 
                        'stars': '1340', 
                        'usercoins': '130', 
                        'demons': '40', 
-                       'diamonds': '1919', 
+                       'diamonds': '1919',
+                       'cp': '2',
                        'youtube': 'UC5JsIMU43qQw9ggxdVpJgRg', 
                        'twitter': '@Nerex1', 
                        'twitch': 'NerexGD', 
                        'accountid': '20207462', 
-                       'userid': '5837301' }
+                       'userid': '5837301' } 
+"""        
+                       
+print(Player["username"]) # returns "Nerex"
 
-                
-print(data["username"]) #returns "Nerex"
+
+Level = GDClient.GetLevelInfo("Cataclysm") #You can also use a Level ID and specify a creator! (GDClient.GetLevelInfo("Cataclysm", "GgBoy")
+print(Level["stars"]) # return 10
+
+
+LeaderboardCreators = GDClient.GetCreatorsLeaderboard(100)
+print(LeaderboardCreators[0]["username"]) # returns ViPrIn
